a4dd17bb207c140a13ab2818f86dbf5d914b4ef9
herba13 <biome6364@gmail.com>
2022-10-08 00:03:10 +0900

    ㅋㄷ
    코드썻다

--- PotatoPotatoBug/Assets/Script/potetoMgr.cs
+++ PotatoPotatoBug/Assets/Script/potetoMgr.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class potetoMgr : MonoBehaviour
 {
-    
+    public float moveX;
+
+
+    public float speeeeeed;
     public float moveSpeed;
 
     Rigidbody2D rb;
@@ -19,9 +22,18 @@ public class potetoMgr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("Left"))
-        {
+        /*moveX = Input.GetAxisRaw("Horizontal") * speeeeeed;
 
+        rb.velocity = new Vector2(moveX, rb.velocity.y);*/
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, Time.deltaTime * moveSpeed, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -Time.deltaTime * moveSpeed, Space.Self);
         }
     }
 }
