8d1c263247a8983cc0a8dbfc7ae43db392680843
herba13 <103804074+herba13@users.noreply.github.com>
2022-10-11 18:21:19 +0900

    tt
    씨발

--- PotatoPotatoBug/Assets/Script/potetoMgr.cs
+++ PotatoPotatoBug/Assets/Script/potetoMgr.cs
@@ -1,39 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class potetoMgr : MonoBehaviour
 {
-    public float moveX;
 
 
-    public float speeeeeed;
+
+    
     public float moveSpeed;
 
-    Rigidbody2D rb;
+  
 
 
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        /*moveX = Input.GetAxisRaw("Horizontal") * speeeeeed;
-
-        rb.velocity = new Vector2(moveX, rb.velocity.y);*/
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, 0, Time.deltaTime * moveSpeed, Space.Self);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, 0, -Time.deltaTime * moveSpeed, Space.Self);
-        }
+        transform.Rotate(new Vector3(0, 0, -(transform.rotation.z + moveSpeed) * Input.GetAxisRaw("Horizontal")));
     }
 }
