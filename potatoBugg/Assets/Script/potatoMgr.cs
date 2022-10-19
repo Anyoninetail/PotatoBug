c57489160e6faa363dafee8d429f8a791f25d2ce
herba13 <103804074+herba13@users.noreply.github.com>
2022-10-19 15:28:22 +0900

    ㅅㅅ
    코드수정

--- potatoBugg/Assets/Script/potatoMgr.cs
+++ potatoBugg/Assets/Script/potatoMgr.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class potatoMgr : MonoBehaviour
 {
@@ -9,28 +10,45 @@ public class potatoMgr : MonoBehaviour
     public float jumpPower;
     public float rayleng;
 
+    public GameObject Gmsetting;
+
+
 
     public int jumpcount;
     public int maxJumpCount = 2;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
-    void Start()
+    void Start()    
     {
+        Gmsetting.SetActive(false);
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
 
-        //moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
 
-        //rb.velocity = new Vector2(moveX, rb.velocity.y);
-
-        transform.Rotate(new Vector3(0, 0, -(transform.rotation.z + (moveSpeed / 2)) * Input.GetAxisRaw("Horizontal")));
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+        
+        transform.Rotate(new Vector3(0, 0, -(transform.rotation.z + moveSpeed) * Input.GetAxisRaw("Horizontal")));
 
     }
 
-   
+    public void OnCollision2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            Gmsetting.SetActive(true);
+        }
+    }
+
+    public void Gomane()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
 }
